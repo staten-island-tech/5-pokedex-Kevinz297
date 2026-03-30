a0fdcd247f4aeb6b43b9715fe8b45f0b6aab78ad
Kevinz297 <Kevinz297@nycstudents.net>
2026-03-30 09:26:09 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-import json
+""" import json
 ## Open the JSON file of pokemon data
 pokedex = open("./pokedex.json", encoding="utf8")
 ## create variable "data" that represents the enitre pokedex list
 data = json.load(pokedex)
-print(data[0])
+print(data[0]) """
 
 # Create a function that will take the data from the JSON file and you will iterate through the list of pokemon and print each pokemons name.
 
@@ -15,3 +15,29 @@ print(data[0])
 
 #For Leo/, help me come up with a clever final question, considering maybe showing all moves a pokemon has avaiable based on type
 
+sushi_orders = [
+    {"name": "California Roll", "price": 8},
+    {"name": "Spicy Tuna Roll", "price": 10},
+    {"name": "Salmon Nigiri", "price": 6},
+    {"name": "California Roll", "price": 8},
+    {"name": "Dragon Roll", "price": 12},
+    {"name": "Spicy Tuna Roll", "price": 10},
+    {"name": "Miso Soup", "price": 4},
+    {"name": "Edamame", "price": 5},
+    {"name": "Salmon Nigiri", "price": 6},
+    {"name": "California Roll", "price": 8}
+]
+
+def receipt(orders):
+    receipt= {}
+    for order in orders:
+        if order["name"] in receipt:
+            receipt[order["name"]]["quantity"] += 1
+        else:
+            receipt[order["name"]] ={
+                "price": order["price"],
+                "quantity": 1
+            }
+    print(receipt)
+
+print(receipt(sushi_orders))
